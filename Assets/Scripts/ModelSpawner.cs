82bbc1da57a4251fa1f6afb515e4f8e30a3668e7
Paul Hayes <paul.hayes@entropedia.co.uk>
2015-10-05 16:39:10 +0100

    Initial commit

---
+++ Assets/Scripts/ModelSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelSpawner : MonoBehaviour {
+
+	private ObjImporter importer = new ObjImporter();
+	public Material material;
+	public float distance;
+
+	public void SpawnModel(string info){
+		if(info.Length==0) return;
+		string[] files = info.Split('\n');
+		if( files.Length > 1 ){
+			foreach(var mfile in files){
+				SpawnModel(mfile);
+			}
+			return;
+		}
+		
+		string file = files[0];
+				
+		Debug.Log (info);
+		Mesh mesh = importer.ImportFile(file);
+		GameObject model = new GameObject();
+		GameObject modelContainer = new GameObject();
+		modelContainer.name = file;
+		model.name = "model";
+		
+		MeshFilter meshFilter = model.AddComponent<MeshFilter>();
+		MeshRenderer meshRenderer = model.AddComponent<MeshRenderer>();
+		meshRenderer.material = material;
+		meshFilter.mesh = mesh;
+		Vector3 size = Vector3.Max( meshRenderer.bounds.size, Vector3.one );
+		float max = Mathf.Max(new float[]{ size.x, size.y, size.z });
+		model.transform.localScale = 1f/max * Vector3.one;
+		modelContainer.transform.position = Camera.main.transform.position + distance * Camera.main.transform.forward;
+		model.transform.parent = modelContainer.transform;
+		
+		model.transform.localPosition = -1f * meshRenderer.bounds.center;
+	}
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using UnityEngine.Events;
+
+public class Server : MonoBehaviour 
+{
+	public int port;
+	public MessageEvent onMessage;
+	public enum IPChoices {
+		Loopback,
+		Any,
+		Specific
+	}
+	public IPChoices ip;
+	public string specificIpAddr;
+	
+	protected bool messageReady;
+	protected string message;
+	
+	private TcpListener tcp_Listener = null;
+	private Thread mThread;
+	private bool mRunning;
+	
+	void Awake() {
+		mRunning = true;
+		ThreadStart ts = new ThreadStart(Receive);
+		mThread = new Thread(ts);
+		mThread.Start();
+		print("Thread done...");
+	}
+	
+	public void stopListening() {
+		mRunning = false;
+	}
+	
+	void Receive() {
+		IPAddress addr = IPAddress.Loopback;
+		switch(ip){
+			case IPChoices.Any:
+				addr = IPAddress.Any;
+			break;
+			case IPChoices.Specific:
+				addr = IPAddress.Parse(specificIpAddr);				
+			break;
+		}
+		
+		tcp_Listener = new TcpListener(addr, port);
+		tcp_Listener.Start();
+		print("Server Start");
+		while (mRunning)
+		{
+			// check if new connections are pending, if not, be nice and sleep 100ms
+			if (!tcp_Listener.Pending()){
+				Thread.Sleep(100);
+			}
+			else {
+				Socket ss = tcp_Listener.AcceptSocket();
+				byte[] tempbuffer = new byte[10000];
+				int length = ss.Receive(tempbuffer); // received byte array from client
+				message = System.Text.Encoding.Default.GetString(tempbuffer,0,length);
+				//message = GetString(tempbuffer,length);
+				messageReady = true;
+				ss.Send( GetBytes("OK\n\n") );
+				ss.Close();
+			}
+		}
+	}
+	
+	void Update() {
+		if( messageReady ){
+			messageReady = false;
+			onMessage.Invoke(message);
+		}
+	}
+	
+	void OnApplicationQuit() { // stop listening thread
+		stopListening();// wait for listening thread to terminate (max. 500ms)
+		if( mThread != null ) mThread.Join(500);
+	}
+	
+	static byte[] GetBytes(string str)
+	{
+		byte[] bytes = new byte[str.Length * sizeof(char)];
+		System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+		return bytes;
+	}
+	
+	static string GetString(byte[] bytes, int length)
+	{
+		char[] chars = new char[length / sizeof(char)];
+		System.Buffer.BlockCopy(bytes, 0, chars, 0, length);
+		return new string(chars);
+	}
+}
+
+[System.Serializable]
+public class MessageEvent : UnityEvent<string> 
+{
+
+}
